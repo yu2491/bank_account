e47bdfc0752f3d136c27442569d48d7523e14f44
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-07 11:30:38 +0100

    failed responding to deposit method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,5 @@
 class BankAccount
   def balance
+    0
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -8,13 +8,19 @@ describe BankAccount do
   end
 
   describe "#balance" do
-    it "can view balance" do
+    it "can respond to balance" do
       account = BankAccount.new
       expect(account).to respond_to(:balance)
     end
 
-    it "returns balance" do
-      expect(subject.balance).to be
+    it "should show zero balance initially" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe "#deposit" do
+    it "can respond to deposit" do
+      expect(subject).to respond_to(:deposit)
     end
   end
 end
